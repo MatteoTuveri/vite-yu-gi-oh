2b12dd443e28d0630cbe614a9fd1b7375ca1c9cb
MatteoTuveri <matteo.tuveri16@gmail.com>
2023-11-13 15:42:29 +0100

    initial scaffolding

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+
+  </header>
+  <main>
+    
+  </main>
+</template>
+<script>
+export default{
+  data() {
+    return {
+      
+    }
+  },
+}
+
+</script>
+<style lang="scss" scoped>
+
+</style>
