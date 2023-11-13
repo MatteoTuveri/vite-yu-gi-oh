d7338c3a58636933b57c58ac52423d9afc66388a
MatteoTuveri <matteo.tuveri16@gmail.com>
2023-11-13 16:59:50 +0100

    Add graphic

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,41 @@
 <template>
-  <header>
+  <header class="text-center my-4">
     <h1>Yu-Gi-Oh API</h1>
   </header>
-  <main class="container">
-    <div class="row">
-
-      <CardList/>
-
+  <main class="bg-or">
+    <div class="container py-5">
+      <div class="row">
+        <CardList />
+      </div>
     </div>
   </main>
 </template>
 <script>
-  import { store } from './data/store.js';
-  import axios from 'axios';
-  import CardList from './components/CardList.vue';
+import { store } from './data/store.js';
+import axios from 'axios';
+import CardList from './components/CardList.vue';
 
-export default{
-  name:'App',
+export default {
+  name: 'App',
   data() {
     return {
       store
     }
   },
-  components:{CardList},
+  components: { CardList },
   methods: {
     getCards() {
       const url = store.apiUrl + store.endPoint;
       axios.get(url).then((response) => {
         const cards = response.data.data
-        for(let i=0;i<cards.length;i++){
+        console.log(cards)
+        for (let i = 0; i < cards.length; i++) {
           store.cardList.push({
-          image: cards[i].card_images[0].image_url,
-          name: cards[i].name,
-          type: cards[i].archetype
-        })
-      }
+            image: cards[i].card_images[0].image_url,
+            name: cards[i].name,
+            type: cards[i].type
+          })
+        }
       })
     }
   },
@@ -45,5 +46,7 @@ export default{
 
 </script>
 <style lang="scss" scoped>
-
+.bg-or {
+  background-color: rgb(212, 143, 56);
+}
 </style>
