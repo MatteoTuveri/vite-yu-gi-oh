395cced85394141bfdbd0dc6545c20d867befd6d
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-11-15 11:14:16 +0100

    Add fontawesome

--- src/components/SelectionComp.vue
+++ src/components/SelectionComp.vue
@@ -1,6 +1,6 @@
 <template>
     <select class="form-select my-5" aria-label="Default select example" v-model="search" @change="searchArchetype">
-        <option selected value="">Open this select menu</option>
+        <option selected value="">Scegli l'archetipo</option>
         <option v-for="(item,index) in store.searchList" :value="item.name">{{ item.name }}</option>
     </select>
 </template>
